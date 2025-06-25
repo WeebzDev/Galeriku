1022f3470b7bb939379eb6b5eec19b8f3ce9a4d1
CookyNdi <cookyndi2@gmail.com>
2025-06-25 13:19:10 +0700

    dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <div>
       <Button>UwU</Button>
+      <ThemeToggle />
     </div>
   );
 }
