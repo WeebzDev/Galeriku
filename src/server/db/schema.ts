0a1200f00ff349d766a88d2af18a84742b94b2e4
CookyNdi <cookyndi2@gmail.com>
2025-06-25 13:13:56 +0700

    database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from "drizzle-orm";
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { index, pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -11,19 +11,49 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = pgTableCreator(
-  (name) => `galeriku-weebzdev_${name}`,
+  (name) => `galeriku_${name}`,
 );
 
-export const posts = createTable(
-  "post",
+export const users = createTable(
+  "users",
   (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+    id: d.varchar().primaryKey(),
+    username: d.varchar({ length: 256 }).notNull(),
+    password: d.varchar({ length: 256 }).notNull(),
   }),
-  (t) => [index("name_idx").on(t.name)],
+  (t) => [index("username_idx").on(t.username)],
+);
+
+export const images = createTable("images", (d) => ({
+  id: d.varchar({ length: 256 }).primaryKey(),
+  userId: d.varchar({ length: 256 }).notNull(),
+  name: d.varchar({ length: 256 }).notNull(),
+  uniqueName: d.varchar({ length: 256 }).notNull(),
+  fileSize: d.bigint({ mode: "number" }).notNull(),
+  url: d.varchar({ length: 256 }).notNull(),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+}));
+
+export const tags = createTable("tags", (d) => ({
+  id: d.varchar({ length: 256 }).primaryKey(),
+  name: d.varchar({ length: 256 }).notNull(),
+}));
+
+export const imageTags = createTable(
+  "image_tags",
+  (d) => ({
+    imageId: d
+      .varchar("imageId")
+      .notNull()
+      .references(() => images.id, { onDelete: "cascade" }),
+    tagId: d
+      .varchar("tagId")
+      .notNull()
+      .references(() => tags.id, { onDelete: "cascade" }),
+  }),
+  (t) => [primaryKey({ columns: [t.imageId, t.tagId] })],
 );
