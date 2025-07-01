2bac0768803b1b9003a15abffeaa3a860ef10055
CookyNdi <cookyndi2@gmail.com>
2025-07-01 11:14:23 +0700

    Set revalidate to 0 for HomePage
    Added 'export const revalidate = 0;' to disable ISR and ensure the HomePage is always rendered on each request.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,8 @@ import { MainContainer } from "@/components/container/main-container";
 import { ImagesContainer } from "@/components/image/container";
 import { QUERIES } from "@/server/db/queries";
 
+export const revalidate = 0;
+
 export default async function HomePage() {
   const [images] = await Promise.all([QUERIES.getAllImages()]);
 
