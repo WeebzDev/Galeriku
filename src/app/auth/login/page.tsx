de9c5bf17354c133b73b29e1ade657fae2ceecb6
CookyNdi <cookyndi2@gmail.com>
2025-07-14 19:41:47 +0700

    Add demo credentials to login page
    Displayed demo username and password below the login form to assist users in testing the login functionality.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,6 +5,11 @@ export default function LoginPage() {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
+        <div className="flex justify-center flex-col items-center pt-4">
+          <p>Demo</p>
+          <p>Username : dropio</p>
+          <p>Password : Password!123</p>
+        </div>
       </div>
     </div>
   );
